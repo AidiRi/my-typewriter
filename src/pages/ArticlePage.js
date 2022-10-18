4adb8d72c98ebdb795b61af03c186124d2c250e0
AidiRi <rivera.aidi@gmail.com>
2022-10-18 16:23:00 -0700

    refactoring articlePage display code

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -7,14 +7,19 @@ const ArticlePage = () => {
 	// Destructuring of:
 	// 	const params = useParams();
 	// 	const articleId = params.articleId;
+
 	const article = articles.find(article => article.name === articleId);
+	// Best way to display code?: explicit/content jsx INSIDE return || implicit/content jsx OUTSIDE return??
+	const content = article => {
+		return article.content.map(paragraph => (
+			<p>{paragraph}</p>
+		));
+	};
 
 	return (
 		<>
 			<h1>{article.title}</h1>
-			{article.content.map(paragraph => (
-			<p>{paragraph}</p>
-			))}
+			{content(article)}
 		</>
 		);
 }
