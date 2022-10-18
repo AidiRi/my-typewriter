2cbe9b0d0005b9d0bc881cc6be56257ed08ae5dc
AidiRi <rivera.aidi@gmail.com>
2022-10-18 16:07:20 -0700

    rendering article content in ArticlePage component

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,7 +1,21 @@
 //Functional
+import { useParams } from 'react-router-dom';
+import articles from './article-content.js';
+
 const ArticlePage = () => {
+	const { articleId } = useParams();
+	// Destructuring of:
+	// 	const params = useParams();
+	// 	const articleId = params.articleId;
+	const article = articles.find(article => article.name === articleId);
+
 	return (
-			<h1>This is the article page</h1>
+		<>
+			<h1>{article.title}</h1>
+			{article.content.map(paragraph => (
+			<p>{paragraph}</p>
+			))}
+		</>
 		);
 }
 
