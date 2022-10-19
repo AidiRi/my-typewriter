596f3e1eebef4a080edcf3a4e13b94ab0140a520
AidiRi <rivera.aidi@gmail.com>
2022-10-19 15:14:48 -0700

    importing articles to display on ArticlesListPage

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,7 +1,17 @@
 // Functional
+import articles from './article-content';
+
 const ArticlesListPage = () => {
 	return (
-			<h1>This is the articles list page</h1>
+		<>
+			<h1>Articles</h1>
+			{articles.map( article =>
+				<div>
+					<h3>{article.title}</h3>
+					<p>{article.content[0].substring(0, 150)}...</p>
+				</div>
+			)}
+		</>
 		);
 }
 
